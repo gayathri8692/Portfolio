923df95d51130f17f7b792c1714e7c17af4d35a0
gayathri <gayathri.07310@gmail.com>
2017-06-26 18:49:50 -0400

    almost done

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const express = require('express');
-const app     = express();
-const port    = process.env.PORT || 3000;
+const app = express();
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+//routes import
+var index = require('./routes/index.js');
+var projects = require('./routes/projects.js');
+var contact = require('./routes/contact.js');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//for sass
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
+
+//use routes
+app.use('/', index);
+app.use('/projects', projects);
+app.use('/contact', contact);
+
+//set up PORT
+const port = process.env.PORT || 3000;
 app.listen(port);
-console.log(`app running on port ${port}...`);
+console.log(`App running on port ${port}...`);
+
+module.exports = app;
---
+++ routes/contact.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res) {
+  res.render("contact", {
+    title: "Contact"
+  });
+});
+
+module.exports = router;
